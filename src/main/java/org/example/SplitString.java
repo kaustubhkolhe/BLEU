c927fb7a7d1bad2ee1c6fed7cc845525ef569fe0
kaustubhkolhe <kaustubhkolhe34@gmail.com>
2024-03-15 16:06:09 +0530

    made changes as per sacrebleu logic

---
+++ src/main/java/org/example/SplitString.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SplitString {
+    public static void main(String[] args) {
+        String sentence = "I can't wait. for the start of the weekend .";
+
+
+        sentence = sentence.trim();
+
+
+        String[] words = sentence.split("(?=[.,!?])|(?<=[.,!?])|\\s+");
+
+        // Filter out empty strings using Stream
+        ArrayList<String> result = Arrays.stream(words)
+                .filter(word -> !word.isEmpty())
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println(result);
+        System.out.println(result.size());
+    }
+}
+
