c102d5513c706a889df3c8fadee59cf92c33bcde
kaustubhkolhe <kaustubhkolhe34@gmail.com>
2024-03-15 16:28:21 +0530

    trim combined with split

--- src/main/java/org/example/SplitString.java
+++ src/main/java/org/example/SplitString.java
@@ -8,8 +8,8 @@ public class SplitString {
     public static void main(String[] args) {
         String sentence = "I can't wait. for the start of the weekend .";
 
-        sentence = sentence.trim();
-        String[] words = sentence.split("(?=[.,!?])|(?<=[.,!?])|\\s+");
+
+        String[] words = sentence.trim().split("(?=[.,!?])|(?<=[.,!?])|\\s+");
 
         // Filter out empty strings using Stream
         ArrayList<String> result = Arrays.stream(words)
